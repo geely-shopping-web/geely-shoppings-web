63a23ea86959bf43c53f2da4e8293e6c12163ca4
WenShunhao <58215914+WenShunhao@users.noreply.github.com>
2020-02-28 16:01:21 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,17 @@
+		
+
+$(".floor_goods_wrap_li").hover(function () {
+       $(this).css({"top":"-5px",
+           "box-shadow":"0px 15px 30px rgba(0,0,0,0.2)"
+       });
+},function(){
+    $(this).css({"top":"0px",
+        "box-shadow":"none"
+    });
+})
+
+$(".foot_bottom_r").children("span").hover(function(){
+    $(this).css({"background":"#ff6700",color:"#fff"});
+},function(){
+    $(this).css({"background":"none",color:"#ff6700"});
+})
